70ba19e80de7e64415603316dfcd1b12be0e1761
Joosemi02 <josemiguel.dark@gmail.com>
2023-11-09 12:29:47 +0100

    - Add cats and chances - Add default_timeout

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,3 +1,37 @@
+# CHANCE
+CAT_CHANCES = {
+    "ash": 1,
+    "azure": 1,
+    "beige": 1,
+    "black": 1,
+    "blue": 1,
+    "brown": 1,
+    "brownie": 1,
+    "calico": 1,
+    "carrot": 1,
+    "chocolate": 1,
+    "cinnamon": 1,
+    "cloud": 1,
+    "coal": 1,
+    "cocoa": 1,
+    "coffee": 1,
+    "cream": 1,
+    "garfield": 1,
+    "gray": 1,
+    "jade": 1,
+    "latte": 1,
+    "navy": 1,
+    "obsidian": 1,
+    "ocean": 1,
+    "oreo": 1,
+    "rust": 1,
+    "sand": 1,
+    "snow": 1,
+    "storm": 1,
+    "truffle": 1,
+    "white": 1,
+}
+
 # ANIMATIONS
 FRAME_SCHEME = (
     (7, 6, 6, 6, 7, 6, 6, 6),
@@ -13,3 +47,9 @@ BIG_RESOLUTION = 265
 # COLORS
 BLUE = 0x3498DB
 EMBED_COLOR = 0x3498DB
+
+# TIMEOUTS
+DEFAULT_TIMEOUT = 30
+
+if __name__ == "__main__":
+    print(f"Default cat chance: {100/sum(CAT_CHANCES.values())}%")
