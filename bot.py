0d7dcc4f3342086e6f10a803fe9d6cb57f18e631
Joosemi02 <josemiguel.dark@gmail.com>
2023-11-09 11:05:03 +0100

    Move catbot class to helpers.utils

--- bot.py
+++ bot.py
@@ -1,56 +1,11 @@
-import logging
 import os
-from logging.handlers import RotatingFileHandler
 
 import discord
 import uvicorn
 from discord.ext import commands
 from dotenv import load_dotenv
 
-import cogs
-from helpers import constants
-
-
-class CatBot(commands.Bot):
-    class BlueEmbed(discord.Embed):
-        def __init__(self, **kwargs):
-            color = kwargs.pop("color", constants.BLUE)
-            super().__init__(**kwargs, color=color)
-
-    class Embed(discord.Embed):
-        def __init__(self, **kwargs):
-            color = kwargs.pop("color", constants.EMBED_COLOR)
-            super().__init__(**kwargs, color=color)
-
-    def setup_logging(self):
-        logging_format = logging.Formatter(
-            "{asctime} {levelname:<8}: {message}",
-            "%d-%m-%Y %H:%M:%S",
-            "{",
-        )
-        self.log = logger = logging.getLogger()
-        logger.setLevel(logging.INFO)
-
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(logging_format)
-        console_handler.setLevel(logging.INFO)
-        logger.addHandler(console_handler)
-
-        file_handler = RotatingFileHandler(
-            filename="discord.log",
-            encoding="utf-8",
-            maxBytes=5 * 1024 * 1024,  # 5 Mb
-            backupCount=3,
-        )
-        file_handler.setFormatter(logging_format)
-        file_handler.setLevel(logging.WARN)
-        logger.addHandler(file_handler)
-
-    async def setup_hook(self):
-        for i in cogs.default:
-            await self.load_extension(f"cogs.{i}")
-        self.log.info("Bot loaded")
-
+from helpers.utils import CatBot
 
 intents = discord.Intents.default()
 bot = CatBot(
@@ -67,11 +22,12 @@ async def sync(ctx: commands.Context):
     await ctx.send("synced")
 
 
-def main(bot):
+def main(bot: CatBot):
     load_dotenv()
 
     bot.setup_logging()
     bot.run(os.getenv("BOT_TOKEN"), log_handler=None)
+
 
 if __name__ == "__main__":
     uvicorn.main(main(bot))
