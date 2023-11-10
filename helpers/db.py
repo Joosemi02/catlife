a088ae12a9210586bbf651e560aa4351fbf82f97
Joosemi02 <josemiguel.dark@gmail.com>
2023-11-10 11:52:34 +0100

    Add cats collection

--- helpers/db.py
+++ helpers/db.py
@@ -6,3 +6,4 @@ from motor import motor_tornado
 load_dotenv()
 client = motor_tornado.MotorClient(os.getenv("MONGODB_URI"))
 db: motor_tornado.core.AgnosticDatabase = client["catlife"]
+CATS = db.cats
