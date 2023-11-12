3b69688cfa70937914b52c483d582a3c6e51ed45
Joosemi02 <josemiguel.dark@gmail.com>
2023-11-12 19:21:41 +0100

    - Remove CatBot.BlueEmbed - Edit CatBot.Embed to show docstrings - Create Cat class - Create generate_cat - Create get_random_stats

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,28 +1,113 @@
 import logging
+import random
+from datetime import datetime
 from logging.handlers import RotatingFileHandler
+from typing import Self
 
-from discord import Embed, app_commands
+from discord import Embed, User, app_commands
 from discord.ext import commands
 from discord.interactions import Interaction
 
 import cogs
 
-from .constants import BLUE, EMBED_COLOR
+from .constants import BOLT, CAT_CHANCES, EMBED_COLOR, HUNGER
+from .db import CATS
 
 
+class Cat:
+    def __init__(
+        self, variant, born, health, energy, hunger, owner=None, name=None, id=None
+    ):
+        self.name: str = name or variant
+        self.born: datetime = born
+        self.age: str = format_timedelta(born)
+        self.health: int = health
+        self.energy: int = energy
+        self.hunger: int = hunger
+        self.variant: str = variant
+        self.owner: User | None = owner
+        self.id: int | None = id
+
+    def __str__(self) -> str:
+        pass
+
+    def __eq__(self, cat: object) -> bool:
+        return isinstance(cat, Cat) and self.id == cat.id
+
+    async def add_id(self):
+        lowest = CATS.find_one(sort=[("_id", 1)])
+        return lowest["_id"] + 1 or 1
+
+    async def set_owner(self, user: User):
+        self.owner = user
+        await CATS.update_one()
+
+    @classmethod
+    async def from_id(cls, id: int) -> Self:
+        return cls(**await CATS.find_one({"_id": id}))
+
+    @property
+    def embed_field_args(self):
+        return {
+            "name": f"{self.name}",
+            "value": get_stats_string(),
+        }
+
+
+# CATS
+def get_random_stats(variant: str):
+    points = random.randint(60, 200)
+    if variant == "catvariant":
+        points = min(points * 1.2, 200)  # 20% boost
+    energy = points / 2
+    hunger = points / 2
+    return {
+        "born": datetime.now(),
+        "health": 10,
+        "energy": int(energy),
+        "hunger": int(hunger),
+    }
+
+
+async def generate_cat() -> Cat:
+    population = list(CAT_CHANCES.keys())
+    weights = list(CAT_CHANCES.values())
+    cat = random.choices(population, weights)[0]
+    stats = get_random_stats(cat)
+    return Cat(variant=cat, **stats)
+
+
+# FORMAT
+def get_stats_string(cat: Cat):
+    return f"{cat.energy} {BOLT}\n{cat.hunger} {HUNGER}"
+
+
+def singularize(amount, unit):
+    """singularizer(?) - returns a string containing the amount
+    and type of something. The type/unit of item will be pluralized
+    if the amount is greater than one."""
+    return f"{amount} {amount == 1 and f'{unit}' or f'{unit}s'}"
+
+
+def format_timedelta(seconds: int) -> str:
+    """Converts a timedelta's total_seconds() to a humanized string."""
+    mins, secs = divmod(seconds, 60)
+    hrs, mins = divmod(mins, 60)
+    dys, hrs = divmod(hrs, 24)
+    timedict = {"day": dys, "hour": hrs, "minute": mins, "second": secs}
+    cleaned = {k: v for k, v in timedict.items() if v != 0}
+    return " ".join(singularize(v, k) for k, v in cleaned.items())
+
+
+# CLASSES
 class CatBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, tree_cls=CatTree)
 
-    class BlueEmbed(Embed):
-        def __init__(self, **kwargs):
-            color = kwargs.pop("color", BLUE)
-            super().__init__(**kwargs, color=color)
-
     class Embed(Embed):
-        def __init__(self, **kwargs):
+        def __new__(cls, **kwargs):
             color = kwargs.pop("color", EMBED_COLOR)
-            super().__init__(**kwargs, color=color)
+            return super().__new__(**kwargs, color=color)
 
     def setup_logging(self):
         logging_format = logging.Formatter(
