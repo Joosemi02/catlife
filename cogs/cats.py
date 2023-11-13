de0aba22659b62df5b638a10e610386b129e4ffa
Joosemi02 <josemiguel.dark@gmail.com>
2023-11-13 02:20:47 +0100

    Create start command draft

--- cogs/cats.py
+++ cogs/cats.py
@@ -1,8 +1,9 @@
 import logging
 
+from discord import Interaction, app_commands
 from discord.ext import commands
 
-from helpers.utils import CatBot
+from helpers.utils import CatBot, generate_cat, merge_cat_images
 
 
 class Cats(commands.Cog):
@@ -12,6 +13,18 @@ class Cats(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         logging.info(f"Cog {self.qualified_name} loaded")
+
+    @app_commands.command()
+    async def start(self, i: Interaction):
+        embed = self.bot.Embed(title="Pick your starter cat")
+        cats = []
+        while len(cats) >3:
+            cat = generate_cat()
+            if cat not in cats:
+                cats.append(cat)
+                embed.add_field(**cat.embed_field_args)
+        embed.set_image(url="attachment://attachment.gif")
+        await i.followup.send(embed=embed, file=merge_cat_images(cats))
 
 
 async def setup(bot):
